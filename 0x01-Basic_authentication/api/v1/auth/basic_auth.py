a5cb62a5a7323331086c922ee217d3c0b84a9d80
koome <koomemc@gmail.com>
2024-04-16 02:55:02 +0300

    extract b64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,10 +3,20 @@
 BasicAuth Module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """
     Basic Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if isinstance(authorization_header, str) is False:
+            return None
+        if authorization_header.split()[0] != "Basic":
+            return None
+        b_64 = authorization_header[len("Basic "):]
+        return b_64
