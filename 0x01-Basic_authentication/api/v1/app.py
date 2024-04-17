58a5b2d4e05e3bd6d5b8389926e7956da0ccbd9a
koome <koomemc@gmail.com>
2024-04-17 16:37:37 +0300

    documentation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -48,6 +48,9 @@ def forbidden(error) -> str:
 
 @app.before_request
 def filter_requests() -> Optional[str]:
+    """
+    filter requests before going to paths
+    """
     if auth:
         excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                           '/api/v1/forbidden/']
