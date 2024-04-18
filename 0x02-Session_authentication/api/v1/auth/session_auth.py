b179d1926c4e7a8e4e10c3b8151e642dcf3fd4f2
koome <koomemc@gmail.com>
2024-04-19 00:25:38 +0300

    fixes

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,11 @@ class SessionAuth(Auth):
             session_id = str(uuid.uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        returns user id based on session_id
+        """
+        if session_id is None or isinstance(session_id, str) is False:
+            return None
+        return self.user_id_by_session_id.get(session_id)
