361cd1933c464f524d64211d7ac8de4e9d903b42
koome <koomemc@gmail.com>
2024-04-19 00:11:33 +0300

    create session id for user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Module: Session Auth
 session authentiation
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -11,3 +12,17 @@ class SessionAuth(Auth):
     inherits from auth to
     create sessions
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates session id for user id
+        """
+        if not isinstance(user_id, str):
+            return None
+        elif user_id is None:
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
