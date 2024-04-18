67f59e13d9276e092bef9c8eac1d06bc6afa4b5f
koome <koomemc@gmail.com>
2024-04-18 23:58:47 +0300

    shebang

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,4 +1,8 @@
-"""Module Session Auth"""
+#!/usr/bin/env python3
+"""
+Module: Session Auth
+session authentiation
+"""
 from api.v1.auth.auth import Auth
 
 
@@ -7,4 +11,3 @@ class SessionAuth(Auth):
     inherits from auth to
     create sessions
     """
-    pass
