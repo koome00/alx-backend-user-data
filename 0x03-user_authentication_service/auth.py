448ed4d22ef9922c8a9c10b8e4b8ca0af11dd40c
koome <koomemc@gmail.com>
2024-04-26 02:27:27 +0300

    add exception for update_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,5 +67,5 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
-        except (NoResultFound, InvalidRequestError):
+        except (NoResultFound, InvalidRequestError, ValueError):
             return None
