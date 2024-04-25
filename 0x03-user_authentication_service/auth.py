1997ce6a55fbaf1b07d8f7daf0604a65118172e0
koome <koomemc@gmail.com>
2024-04-26 02:10:32 +0300

    generate uuid4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -17,6 +18,13 @@ def _hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode("utf-8"),
                            bcrypt.gensalt())
     return hashed
+
+
+def _generate_uuid() -> str:
+        """
+        generate uuid
+        """
+        return str(uuid.uuid4())
 
 
 class Auth:
@@ -49,3 +57,5 @@ class Auth:
                 return bcrypt.checkpw(password.encode(), hashed)
         except (InvalidRequestError, InvalidRequestError):
             return False
+
+    
