a50a7d4035b34ca2ab3c5a8fc9b5ccbf207757cc
koome <koomemc@gmail.com>
2024-04-25 21:42:07 +0300

    user model

---
+++ 0x03. User authentication service/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""""
+Module 1: Create a class that creates a db table
+"""
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String, Integer
+
+
+class Base(DeclarativeBase):
+    """
+    Declarative base class
+    """
+    pass
+
+
+class User(Base):
+    """
+    Declare a table called users
+    """
+    __tablename__ = "users"
+    id = mapped_column(Integer, primary_key=True)
+    email = mapped_column(String, nullable=False)
+    hashed_password = mapped_column(String)
+    session_id = mapped_column(String, nullable=True)
+    reset_token = mapped_column(String, nullable=True)
