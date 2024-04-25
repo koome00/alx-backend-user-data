9c6040e6ceb0f7d5ecc7b5b972f1a8711f72f573
koome <koomemc@gmail.com>
2024-04-26 01:17:17 +0300

    register users method using their hahed pass

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Module 4: Auth
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +16,23 @@ def _hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode("utf-8"),
                            bcrypt.gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        function to register user
+        """
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError("User <user's email> already exists")
+        except NoResultFound:
+            hashed = _hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=hashed)
+            return user
