f271af738c66768186de141941a186d738174c99
koome <koomemc@gmail.com>
2024-04-25 21:48:51 +0300

    use sqlalchemy 1.3.x

--- 0x03. User authentication service/user.py
+++ 0x03. User authentication service/user.py
@@ -2,16 +2,11 @@
 """"
 Module 1: Create a class that creates a db table
 """
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import mapped_column
-from sqlalchemy import String, Integer
+from tkinter.tix import COLUMN
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
-
-class Base(DeclarativeBase):
-    """
-    Declarative base class
-    """
-    pass
+Base = declarative_base()
 
 
 class User(Base):
@@ -19,8 +14,8 @@ class User(Base):
     Declare a table called users
     """
     __tablename__ = "users"
-    id = mapped_column(Integer, primary_key=True)
-    email = mapped_column(String, nullable=False)
-    hashed_password = mapped_column(String)
-    session_id = mapped_column(String, nullable=True)
-    reset_token = mapped_column(String, nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
