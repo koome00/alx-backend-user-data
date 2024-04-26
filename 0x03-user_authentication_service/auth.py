08f1e05e2759d3c01e13a7a85c7389c7147c2b2e
koome <koomemc@gmail.com>
2024-04-26 03:19:24 +0300

    get user from ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,16 @@ class Auth:
             return session_id
         except (NoResultFound, InvalidRequestError, ValueError):
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+        """
+        takes session_id and returns user
+        or none
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except (InvalidRequestError, NoResultFound):
+            return None
